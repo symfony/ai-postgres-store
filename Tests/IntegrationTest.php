Guikingone/symfony-ai

fix(postgres): fix on stored lang + StoreFactory

src/store/src/Bridge/Postgres/Tests/IntegrationTest.php
@@ -12,7 +12,7 @@
 namespace Symfony\AI\Store\Bridge\Postgres\Tests;
 
 use PHPUnit\Framework\Attributes\Group;
-use Symfony\AI\Store\Bridge\Postgres\Store;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
 use Symfony\AI\Store\StoreInterface;
 use Symfony\AI\Store\Test\AbstractStoreIntegrationTestCase;
 
@@ -26,7 +26,7 @@ final class IntegrationTest extends AbstractStoreIntegrationTestCase
     {
         $pdo = new \PDO('pgsql:host=127.0.0.1;port=5432;dbname=test_database', 'postgres', 'postgres');
 
-        return new Store($pdo, 'test_vectors');
+        return StoreFactory::createFromPDO($pdo, 'test_vectors');
     }
 
     protected static function getSetupOptions(): array

src/store/src/Bridge/Postgres/Tests/StoreFactoryTest.php
@@ -1,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Store\Bridge\Postgres\Tests;
+
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
+use Symfony\AI\Store\Exception\InvalidArgumentException;
+
+final class StoreFactoryTest extends TestCase
+{
+    public function testFromDbalWithNonPdoDriverThrowsException()
+    {
+        $connection = $this->createMock(Connection::class);
+
+        $connection->expects($this->once())
+            ->method('getNativeConnection')
+            ->willReturn(new \stdClass());
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Only DBAL connections using PDO driver are supported.');
+        $this->expectExceptionCode(0);
+        StoreFactory::createFromDbal($connection, 'test_table');
+    }
+}

src/store/src/Bridge/Postgres/Tests/StoreTest.php
@@ -16,9 +16,10 @@ use PHPUnit\Framework\TestCase;
 use Symfony\AI\Platform\Vector\Vector;
 use Symfony\AI\Store\Bridge\Postgres\Distance;
 use Symfony\AI\Store\Bridge\Postgres\Store;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
 use Symfony\AI\Store\Document\Metadata;
 use Symfony\AI\Store\Document\VectorDocument;
-use Symfony\AI\Store\Exception\InvalidArgumentException;
+use Symfony\AI\Store\Exception\RuntimeException;
 use Symfony\AI\Store\Query\HybridQuery;
 use Symfony\AI\Store\Query\TextQuery;
 use Symfony\AI\Store\Query\VectorQuery;
@@ -462,7 +463,7 @@ final class StoreTest extends TestCase
     {
         $pdo = $this->createMock(\PDO::class);
 
-        $store = Store::fromPdo($pdo, 'test_table', 'vector_field');
+        $store = StoreFactory::createFromPDO($pdo, 'test_table', 'vector_field');
 
         $this->assertInstanceOf(Store::class, $store);
     }
@@ -476,23 +477,9 @@ final class StoreTest extends TestCase
             ->method('getNativeConnection')
             ->willReturn($pdo);
 
-        $store = Store::fromDbal($connection, 'test_table', 'vector_field');
+        $store = StoreFactory::createFromDbal($connection, 'test_table', 'vector_field');
 
         $this->assertInstanceOf(Store::class, $store);
-    }
-
-    public function testFromDbalWithNonPdoDriverThrowsException()
-    {
-        $connection = $this->createMock(Connection::class);
-
-        $connection->expects($this->once())
-            ->method('getNativeConnection')
-            ->willReturn(new \stdClass());
-
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Only DBAL connections using PDO driver are supported.');
-
-        Store::fromDbal($connection, 'test_table');
     }
 
     public function testQueryWithNullMetadata()
@@ -880,6 +867,12 @@ final class StoreTest extends TestCase
 
     private function normalizeQuery(string $query): string
     {
-        return trim(preg_replace('/\s+/', ' ', $query));
+        $replacedQuery = preg_replace('/\s+/', ' ', $query);
+
+        if (null === $replacedQuery) {
+            throw new RuntimeException('Query cannot be normalized.');
+        }
+
+        return trim($replacedQuery);
     }
 }

symfony/ai-postgres-store

Tests/IntegrationTest.php
@@ -12,7 +12,7 @@
 namespace Symfony\AI\Store\Bridge\Postgres\Tests;
 
 use PHPUnit\Framework\Attributes\Group;
-use Symfony\AI\Store\Bridge\Postgres\Store;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
 use Symfony\AI\Store\StoreInterface;
 use Symfony\AI\Store\Test\AbstractStoreIntegrationTestCase;
 
@@ -26,7 +26,7 @@ final class IntegrationTest extends AbstractStoreIntegrationTestCase
     {
         $pdo = new \PDO('pgsql:host=127.0.0.1;port=5432;dbname=test_database', 'postgres', 'postgres');
 
-        return new Store($pdo, 'test_vectors');
+        return StoreFactory::createStoreFromPDO($pdo, 'test_vectors');
     }
 
     protected static function getSetupOptions(): array

Tests/StoreFactoryTest.php
@@ -1,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Store\Bridge\Postgres\Tests;
+
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\TestCase;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
+use Symfony\AI\Store\Exception\InvalidArgumentException;
+
+final class StoreFactoryTest extends TestCase
+{
+    public function testFromDbalWithNonPdoDriverThrowsException()
+    {
+        $connection = $this->createMock(Connection::class);
+
+        $connection->expects($this->once())
+            ->method('getNativeConnection')
+            ->willReturn(new \stdClass());
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Only DBAL connections using PDO driver are supported.');
+        $this->expectExceptionCode(0);
+        StoreFactory::createStoreFromDbal($connection, 'test_table');
+    }
+}

Tests/StoreTest.php
@@ -16,9 +16,10 @@ use PHPUnit\Framework\TestCase;
 use Symfony\AI\Platform\Vector\Vector;
 use Symfony\AI\Store\Bridge\Postgres\Distance;
 use Symfony\AI\Store\Bridge\Postgres\Store;
+use Symfony\AI\Store\Bridge\Postgres\StoreFactory;
 use Symfony\AI\Store\Document\Metadata;
 use Symfony\AI\Store\Document\VectorDocument;
-use Symfony\AI\Store\Exception\InvalidArgumentException;
+use Symfony\AI\Store\Exception\RuntimeException;
 use Symfony\AI\Store\Query\HybridQuery;
 use Symfony\AI\Store\Query\TextQuery;
 use Symfony\AI\Store\Query\VectorQuery;
@@ -462,7 +463,7 @@ final class StoreTest extends TestCase
     {
         $pdo = $this->createMock(\PDO::class);
 
-        $store = Store::fromPdo($pdo, 'test_table', 'vector_field');
+        $store = StoreFactory::createStoreFromPDO($pdo, 'test_table', 'vector_field');
 
         $this->assertInstanceOf(Store::class, $store);
     }
@@ -476,23 +477,9 @@ final class StoreTest extends TestCase
             ->method('getNativeConnection')
             ->willReturn($pdo);
 
-        $store = Store::fromDbal($connection, 'test_table', 'vector_field');
+        $store = StoreFactory::createStoreFromDbal($connection, 'test_table', 'vector_field');
 
         $this->assertInstanceOf(Store::class, $store);
-    }
-
-    public function testFromDbalWithNonPdoDriverThrowsException()
-    {
-        $connection = $this->createMock(Connection::class);
-
-        $connection->expects($this->once())
-            ->method('getNativeConnection')
-            ->willReturn(new \stdClass());
-
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Only DBAL connections using PDO driver are supported.');
-
-        Store::fromDbal($connection, 'test_table');
     }
 
     public function testQueryWithNullMetadata()
@@ -880,6 +867,12 @@ final class StoreTest extends TestCase
 
     private function normalizeQuery(string $query): string
     {
-        return trim(preg_replace('/\s+/', ' ', $query));
+        $replacedQuery = preg_replace('/\s+/', ' ', $query);
+
+        if (null === $replacedQuery) {
+            throw new RuntimeException('Query cannot be normalized.');
+        }
+
+        return trim($replacedQuery);
     }
 }